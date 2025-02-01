d530fab8e148cad6df9b19512138a2a511f7bf23
Jayson Miller <jayson.scott.miller@outlook.com>
2025-02-01 22:16:17 +0000

    FEAT: Added initial tests

--- src/com/company/tests/NormaliserTest.java
+++ src/com/company/tests/NormaliserTest.java
@@ -1,7 +1,48 @@
 package com.company.tests;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.company.utility.Normaliser;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class NormaliserTest {
 
+    private Normaliser normaliser;
+
+    @BeforeEach
+    void setUp() {
+        normaliser = new Normaliser();
+    }
+
+    @Test
+    void testCorrectValue() {
+        String jobTitle = "Software engineer";
+        String result = normaliser.normalise(jobTitle);
+        assertEquals("Software engineer", result, "The job title should match 'Software engineer'");
+    }
+
+    @Test
+    void testEmptyInput() {
+        String jobTitle = "";
+        String result = normaliser.normalise(jobTitle);
+        assertEquals("Job title cannot be null or empty", result);
+    }
+
+    @Test
+    void testNullInput() {
+        String result = normaliser.normalise(null);
+        assertEquals("Job title cannot be null or empty", result);
+    }
+
+    @Test
+    void testMisspelledInput() {
+        String jobTitle = "Soft Engineer";
+        String result = normaliser.normalise(jobTitle);
+        assertEquals("Software engineer", result, "The misspelled title should match 'Software engineer'");
+
+        jobTitle = "Accontant";
+        result = normaliser.normalise(jobTitle);
+        assertEquals("Accountant", result, "The misspelled title should match 'Accountant'");
+    }
 }
--- src/com/company/utility/Normaliser.java
+++ src/com/company/utility/Normaliser.java
@@ -2,4 +2,10 @@ package com.company.utility;
 
 public class Normaliser {
 
+    public Normaliser() {
+    }
+
+    public String normalise(final String input) {
+        return "";
+    }
 }
