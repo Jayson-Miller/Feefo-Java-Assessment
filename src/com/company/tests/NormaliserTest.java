33ddbabe0da86b8096cde87b801f0e965de582f3
Jayson Miller <jayson.scott.miller@outlook.com>
2025-02-01 23:10:07 +0000

    FEAT: Extended Initial Tests

--- src/com/company/tests/NormaliserTest.java
+++ src/com/company/tests/NormaliserTest.java
@@ -16,23 +16,44 @@ class NormaliserTest {
     }
 
     @Test
-    void testCorrectValue() {
-        String jobTitle = "Software engineer";
+    void testSoftwareEngineerCorrectValue() {
+        String jobTitle = "Java Engineer";
         String result = normaliser.normalise(jobTitle);
         assertEquals("Software engineer", result, "The job title should match 'Software engineer'");
+    }
+
+    @Test
+    void testArchitectCorrectValue() {
+        String jobTitle = "Senior Building Architect";
+        String result = normaliser.normalise(jobTitle);
+        assertEquals("Architect", result, "The job title should match 'Architect'");
+    }
+
+    @Test
+    void testQuantitySurveyorCorrectValue() {
+        String jobTitle = "Junior Quantity Surveyor";
+        String result = normaliser.normalise(jobTitle);
+        assertEquals("Quantity surveyor", result, "The job title should match 'Quantity surveyor'");
+    }
+
+    @Test
+    void testAccountantCorrectValue() {
+        String jobTitle = "Banking Accountant";
+        String result = normaliser.normalise(jobTitle);
+        assertEquals("Accountant", result, "The job title should match 'Accountant'");
     }
 
     @Test
     void testEmptyInput() {
         String jobTitle = "";
         String result = normaliser.normalise(jobTitle);
-        assertEquals("Job title cannot be null or empty", result);
+        assertEquals("Job Title cannot be null or an empty string", result);
     }
 
     @Test
     void testNullInput() {
         String result = normaliser.normalise(null);
-        assertEquals("Job title cannot be null or empty", result);
+        assertEquals("Job Title cannot be null or an empty string", result);
     }
 
     @Test
